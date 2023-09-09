c5d05e73f2afe366e1f6f642d0244ef650eb3ab7
Navyam-Raushan <125335352+Navyam-Raushan@users.noreply.github.com>
2023-09-10 01:23:51 +0530

    D-26: added date to invoices

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import pandas as pd
 import glob
+import pandas as pd
 from fpdf import FPDF
 from pathlib import Path
 
@@ -10,18 +10,22 @@ filepaths = glob.glob("invoices/*.xlsx")
 # for processing xl files we need openpyxl library of python.
 for filepath in filepaths:
     df = pd.read_excel(filepath, sheet_name="Sheet 1")
+
     # Extracting names of files to use it, firstline will give pure name by removing suffix
     """It will return filename 10001-2023.01.08.xlsx 
     like this.
     """
     filename = Path(filepath).stem
-    invoice_nr = filename.split("-")[0]
+    invoice_nr, date = filename.split("-")
 
     # now generating pdf files.
     pdf = FPDF(orientation="P", unit="mm", format="a4")
     pdf.add_page()
+
     pdf.set_font(family="Times", size=18, style="B")
-    pdf.cell(w=50, h=8, ln=1, txt=f"Invoice nr: {invoice_nr}")
+    pdf.cell(w=50, h=10, ln=1, txt=f"Invoice nr: {invoice_nr}")
+    pdf.cell(w=50, h=10, ln=1, txt=f"Date: {date}")
+
     pdf.output(f"PDFs/{filename}.pdf")
 
 
